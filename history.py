6d6b9e557133a99b183dfd2f205cd854bdbdd13b
Riccardo Massidda <massidda.riccardo@protonmail.com>
2018-09-12 19:05:07 +0200

    First commit

---
+++ history.py
@@ -0,0 +1,90 @@
+import sqlite3
+import ConfigParser
+import os
+
+class FirefoxHistory():
+    def __init__(self):
+        #   Aggregate results
+        self.aggregate = None
+        #   Results order
+        self.order = None
+        #   Results number
+        self.limit = None
+        #   Set history location
+        self.history_location = self.searchPlaces()
+        #   Open Firefox history database
+        self.conn = sqlite3.connect(self.history_location)
+        #   External functions
+        self.conn.create_function('hostname',1,self.__getHostname)
+        #   Observer su history_location per chiamare update_cache
+
+    def searchPlaces(self):
+        #   Firefox folder path
+        firefox_path = os.path.join(os.environ['HOME'], '.mozilla/firefox/')
+        #   Firefox profiles configuration file path
+        conf_path = os.path.join(firefox_path,'profiles.ini') 
+        #   Profile config parse
+        profile = ConfigParser.RawConfigParser()
+        profile.read(conf_path)
+        prof_path = profile.get("Profile0", "Path")
+        #   Sqlite db directory path
+        sql_path = os.path.join(firefox_path,prof_path)
+        #   Sqlite db path
+        return os.path.join(sql_path,'places.sqlite')
+
+    #   Get hostname from url
+    def __getHostname(self,str):
+        url = str.split('/')
+        if len(url)>2:
+            return url[2]
+        else:
+            return 'Unknown'
+
+    def search(self,query_str):
+        #   Aggregate URLs by hostname
+        if self.aggregate == "true":
+            query = 'SELECT hostname(url)'
+        else:
+            query = 'SELECT DISTINCT url'
+
+        query += ',title FROM moz_places WHERE (url LIKE "%%%s%%") OR (title LIKE "%%%s%%")' % (query_str,query_str)    
+        
+        if self.aggregate == "true":
+            query += ' GROUP BY hostname(url) ORDER BY '
+            #   Firefox Frecency
+            if self.order == 'frecency':
+                query += 'sum(frecency)'
+            #   Visit Count
+            elif self.order == 'visit':
+                query += 'sum(visit_count)'
+            #   Last Visit
+            elif self.order == 'recent':
+                query += 'last_visit_date'
+            #   Not sorted
+            else:
+                query += 'hostname(url)'
+        else:
+            query += ' ORDER BY '
+            #   Firefox Frecency
+            if self.order == 'frecency':
+                query += 'frecency'
+            #   Visit Count
+            elif self.order == 'visit':
+                query += 'visit_count'
+            #   Last Visit
+            elif self.order == 'recent':
+                query += 'last_visit_date'
+            #   Not sorted
+            else:
+                query += 'url'
+
+        query += ' DESC LIMIT %d' % self.limit
+
+        #   Query execution
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        return rows
+
+    def close(self):
+        self.conn.close()
---
+++ main.py
@@ -0,0 +1,91 @@
+from ulauncher.api.client.Extension import Extension
+from ulauncher.api.client.EventListener import EventListener
+from ulauncher.api.shared.event import KeywordQueryEvent, SystemExitEvent,PreferencesUpdateEvent, PreferencesEvent
+from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
+from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
+from history import FirefoxHistory
+
+class FirefoxHistoryExtension(Extension):
+    def __init__(self):
+        super(FirefoxHistoryExtension, self).__init__()
+        #   Firefox History Getter
+        self.fh = FirefoxHistory()
+        #   Ulauncher Events
+        self.subscribe(KeywordQueryEvent,KeywordQueryEventListener())
+        self.subscribe(SystemExitEvent,SystemExitEventListener())
+        self.subscribe(PreferencesEvent,PreferencesEventListener())
+        self.subscribe(PreferencesUpdateEvent,PreferencesUpdateEventListener())
+
+class PreferencesEventListener(EventListener):
+    def on_event(self,event,extension):
+        #   Aggregate Results
+        extension.fh.aggregate = event.preferences['aggregate']
+        #   Results Order
+        extension.fh.order = event.preferences['order']
+        #   Results Number
+        try:
+            n = int(event.preferences['limit'])
+        except:
+            n = 10
+        extension.fh.limit = n
+        
+class PreferencesUpdateEventListener(EventListener):
+    def on_event(self,event,extension):
+        #   Results Order
+        if event.id == 'order':
+            extension.fh.order = event.new_value
+        #   Results Number
+        elif event.id == 'limit':
+            try:
+                n = int(event.new_value)
+                extension.fh.limit = n
+            except:
+                pass
+        elif event.id == 'aggregate':
+            extension.fh.aggregate = event.new_value
+
+class SystemExitEventListener(EventListener):
+    def on_event(self,event,extension):
+        extension.fh.close()
+
+class KeywordQueryEventListener(EventListener):
+    def on_event(self, event, extension):
+        query  = event.get_argument()
+        #   Blank Query
+        if query == None:
+            query = ''
+        items = []
+        #   Search into Firefox History
+        results = extension.fh.search(query)
+        for link in results:
+            #   Encode 
+            hostname = link[0].encode("utf-8")
+            #   Split Domain Levels
+            dm = hostname.split('.')
+            #   Remove WWW
+            if dm[0]=='www':
+                i = 1
+            else:
+                i = 0
+            #   Join remaining domains and capitalize
+            name = ' '.join(dm[i:len(dm)-1]).title()
+            #   TODO: favicon of the website
+            if extension.fh.aggregate == "true":
+                items.append(ExtensionResultItem(icon='images/icon.png',
+                                                name=name,
+                                                on_enter=OpenUrlAction('https://'+hostname)))
+            else:
+                if link[1] == None:
+                    title = hostname
+                else:
+                    title = link[1].encode("utf-8")
+                items.append(ExtensionResultItem(icon='images/icon.png',
+                                                name=title,
+                                                description=hostname,
+                                                on_enter=OpenUrlAction(hostname)))
+
+        return RenderResultListAction(items)
+
+if __name__ == '__main__':
+    FirefoxHistoryExtension().run()
